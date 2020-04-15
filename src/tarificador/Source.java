5cf2033cde25d98cd965d37821a362c2ec17d162
Allen Vargas <allenbfox@gmail.com>
2020-04-15 14:23:02 -0400

    Primer Commit
    Inicio del Proyecto

--- src/tarificador/Source.java
+++ src/tarificador/Source.java
@@ -4,7 +4,6 @@ public class Source {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo Soy Allen");
 	}
-
 }
